6c9b72918720496c6160190d01d5e084172b9ffa
rxb <boenigk@gmail.com>
2018-10-30 12:57:15 -0400

    starting upload service

--- packages/frontend/pages/upload.js
+++ packages/frontend/pages/upload.js
@@ -30,7 +30,8 @@ import {
 	Stripe,
 	Text,
 	TextInput,
-	Touch
+	Touch,
+	View
 } from '../components/cinderblock';
 
 
@@ -42,6 +43,11 @@ class Upload extends React.Component {
 
 	static async getInitialProps (context) {
 		return {};
+	}
+
+	constructor(props){
+		super(props);
+		this.state = {}
 	}
 
 	render() {
@@ -67,8 +73,54 @@ class Upload extends React.Component {
 							</Section>
 							<Section>
 								<Chunk>
-								 	<Text>Ok let's do this</Text>
-								 </Chunk>
+									<Label>Your photo</Label>
+									<View style={[styles.input]}>
+										{!this.state.filename &&
+											<Text color="hint">Pick a file</Text>
+										}
+										{this.state.filename &&
+											<Text color="primary">{this.state.filename}</Text>
+										}
+										<input
+											type="file"
+											id="somephoto"
+											onChange={(e)=>{
+												this.setState({
+											      file: URL.createObjectURL(e.target.files[0]),
+											      filename: e.target.value.split(/(\\|\/)/g).pop()
+											    })
+											}}
+											style={{
+												position: 'absolute',
+												opacity: 0,
+												width: '100%',
+												height: '100%'
+											}}
+											/>
+									</View>
+								</Chunk>
+								<Chunk>
+									<Button
+										type="primary"
+										label="Let's do this"
+										onPress={()=>{
+											alert('filez');
+										}}
+										/>
+								</Chunk>
+
+								{this.state.file &&
+									<Chunk>
+										<Image
+											source={{uri: this.state.file}}
+											style={[{
+												height: 300,
+												width: 300
+											}, styles.pseudoLineHeight]}
+											/>
+
+									</Chunk>
+								}
 							</Section>
 						</Sections>
 					</Bounds>
