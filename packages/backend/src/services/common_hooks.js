793b2bc6f0ba9ca9781e28485864e6995e619abb
rxb <boenigk@gmail.com>
2019-10-27 18:50:20 -0400

    shared stuff

---
+++ packages/backend/src/services/common_hooks.js
@@ -0,0 +1,33 @@
+
+
+module.exports = {
+
+  // SAVE AND GET NEW IMAGE REFERENCE
+  saveAndGetNewImageReference: (options = {}) => {
+
+    const opts = {
+      foreignPhotoKey: 'photoId',
+      dataUriKey: 'dataUri', // uplodaed base64 image
+      fileKey: 'file', // multipart file
+      urlKey: 'url', // url of remote image
+      ...options};
+
+    return async (context) => {
+
+      const hasImageToUpload = (context.data[opts.dataUriKey] || context.data[opts.urlKey] || context.params[opts.fileKey] );
+
+      if(hasImageToUpload){
+        const data = {
+          dataUri: context.data[opts.dataUriKey],
+          url: context.data[opts.urlKey]
+        };
+        const params = {
+          file: context.params[opts.fileKey]
+        };
+        const upload = await context.app.service('uploads').create(data, params);
+        context.data[opts.foreignPhotoKey] = upload.id;
+      }
+      return context;
+    }
+  }
+}
---
+++ packages/frontend/components/FeathersClient.js
@@ -0,0 +1,4 @@
+import feathers from '@feathersjs/client';
+const feathersClient = feathers();
+
+export default feathersClient;
