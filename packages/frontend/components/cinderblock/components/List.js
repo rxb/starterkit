789fbfc4ef43d9c1917ff248da35083903abd7af
rxb <boenigk@gmail.com>
2019-11-06 18:30:24 -0500

    card page skeleton

--- packages/frontend/components/cinderblock/components/List.js
+++ packages/frontend/components/cinderblock/components/List.js
@@ -31,7 +31,7 @@ const List = (props) => {
 		itemsInRow,
 		variant = 'linear',
 		items = [],
-		renderItem = ()=>{},
+		renderItem = item => item,
 		media,
 		style,
 		...other
--- packages/frontend/pages/tldr.js
+++ packages/frontend/pages/tldr.js
@@ -2,6 +2,7 @@ import React, {Fragment} from 'react';
 import { connect } from 'react-redux';
 
 import {
+	Avatar,
 	Bounds,
 	Button,
 	Card,
@@ -53,7 +54,7 @@ class Tldr extends React.Component {
 
 # Buster Bluth
 
-*Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud.*
+*Lorem ipsum dolor sit amet, consectetur adipiscing elit*
 
 * **Excepteur sint occaecat cupidatat**
 Non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
@@ -73,52 +74,149 @@ sed do eiusmod tempor incididunt ut labore Okay lets go
 				<Stripe>
 					<Bounds>
 						<Sections>
-							<Section type="pageHead">
 
+							<Flex direction="column" switchDirection="large">
+
+								<FlexItem growFactor={3}>
+
+									<Section>
+
+										<Chunk>
+											<Card style={{
+												borderWidth: 0,
+												borderTopWidth: 12,
+												borderTopColor: 'blue',
+												borderTopStyle: 'solid',
+												shadowRadius: 16,
+												shadowColor: 'rgba(0,0,0,.15)'
+											}}>
+												<Section>
+
+													<Markdown
+														options={{
+												            overrides: {
+												            	h1: {
+												            		component: (props) => (<Chunk>
+												            			<Text type="pageHead">{props.children}</Text>
+												            		</Chunk>)
+												            	},
+												            	h2: {
+												            		component: (props) => (<Chunk>
+												            			<Text type="sectionHead">{props.children}</Text>
+												            		</Chunk>)
+												            	},
+												            	h3: {
+												            		component: (props) => (<Chunk>
+												            			<Text type="big">{props.children}</Text>
+												            		</Chunk>)
+												            	},
+												                p: {
+												                    component: (props) => (<Chunk>
+												            			<Text >{props.children}</Text>
+												            		</Chunk>),
+												                },
+												                li: {
+												                	component: (props) => (<li>{props.children}</li>)
+												                }
+												            },
+												        }}
+														>{markdownContent}</Markdown>
+
+												</Section>
+
+											</Card>
+
+										</Chunk>
+									</Section>
+
+								</FlexItem>
+								<FlexItem growFactor={2}>
+									<Section>
+										<Chunk>
+											<Flex>
+												<FlexItem>
+													<Button
+														label="up / down"
+														width="full"
+														color="secondary"
+														/>
+												</FlexItem>
+												<FlexItem shrink>
+													<Inline style={{flexWrap: 'noWrap'}}>
+														<Button label="a" color="secondary" />
+														<Button label="b" color="secondary" />
+														<Button label="c" color="secondary" />
+													</Inline>
+												</FlexItem>
+											</Flex>
+										</Chunk>
+										<List
+											items={[
+												<Text>References & rationale</Text>,
+												<Text>Issues</Text>,
+												<Text>Contributors & forks</Text>,
+												<Flex>
+													<FlexItem shrink>
+														<Avatar
+															size="medium"
+															source={{uri: 'https://randomuser.me/api/portraits/women/69.jpg'}}
+															/>
+													</FlexItem>
+													<FlexItem>
+														<Text>Maintainer Name</Text>
+														<Text type="small" color="secondary">Maintainer Info</Text>
+													</FlexItem>
+												</Flex>
+											]}
+											renderItem={(item, i)=>{
+												return(
+													<Chunk key={i}>
+														{item}
+													</Chunk>
+												);
+											}}
+											/>
+									</Section>
+
+								</FlexItem>
+							</Flex>
+
+
+							<Section>
 								<Chunk>
-									<Card style={{
-										maxWidth: 500,
-										borderTopWidth: 10,
-										borderTopColor: 'blue',
-										borderTopStyle: 'solid'
-									}}>
-										<Section>
-
-											<Markdown
-												options={{
-										            overrides: {
-										            	h1: {
-										            		component: (props) => (<Chunk>
-										            			<Text type="pageHead">{props.children}</Text>
-										            		</Chunk>)
-										            	},
-										            	h2: {
-										            		component: (props) => (<Chunk>
-										            			<Text type="sectionHead">{props.children}</Text>
-										            		</Chunk>)
-										            	},
-										            	h3: {
-										            		component: (props) => (<Chunk>
-										            			<Text type="big">{props.children}</Text>
-										            		</Chunk>)
-										            	},
-										                p: {
-										                    component: (props) => (<Chunk>
-										            			<Text >{props.children}</Text>
-										            		</Chunk>),
-										                },
-										            },
-										        }}
-												>{markdownContent}</Markdown>
-
-										</Section>
-
-									</Card>
-
+									<Text type="sectionHead">Related cards</Text>
 								</Chunk>
+								<List
+									variant={{
+										small: 'scroll',
+										large: 'grid'
+									}}
+									itemsInRow={4}
+									items={[
+										{title: 'Something is cool', blurb: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit'},
+										{title: 'Something is cool', blurb: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit'},
+										{title: 'Something is cool', blurb: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit'},
+										{title: 'Something is cool', blurb: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit'},
+									]}
+									renderItem={(item, i)=>{
+										return(
+											<Chunk key={i}>
+												<Card>
+													<Section>
+														<Chunk>
+															<Text type="big">{item.title}</Text>
+														</Chunk>
+														<Chunk>
+															<Text>{item.blurb}</Text>
+														</Chunk>
+													</Section>
+												</Card>
+											</Chunk>
+										);
+									}}
+									/>
+
 							</Section>
-
-
 
 						</Sections>
 					</Bounds>
