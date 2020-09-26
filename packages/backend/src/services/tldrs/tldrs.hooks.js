2b97d2bbabed0183830f38f140399d2b123f62ea
Richard <boenigk@gmail.com>
2020-09-26 16:48:24 -0500

    tldr_version hook

--- packages/backend/src/services/tldrs/tldrs.hooks.js
+++ packages/backend/src/services/tldrs/tldrs.hooks.js
@@ -18,7 +18,17 @@ module.exports = {
     ],
     create: [],
     update: [],
-    patch: [],
+    patch: [
+      // special handling if this is a publish
+      async (context) => {
+        if(context.data[publish]){
+          const data = {};
+          const tldr_version = await context.app.service('tldr_versions').create(data);
+          context.data[opts.currentTldrVersionId] = tldr_version.id;
+        }
+        return context;
+      }
+    ],
     remove: []
   },
 
