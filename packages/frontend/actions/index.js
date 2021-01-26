bbc32ec569ec0f9fe0455598f525f6b4e0bcd9c5
Richard <boenigk@gmail.com>
2021-01-26 16:55:10 -0500

    cleaning up redux

--- packages/frontend/actions/index.js
+++ packages/frontend/actions/index.js
@@ -1,53 +1,5 @@
-import { RSAA } from 'redux-api-middleware';
 import {v4 as uuid} from 'uuid';
 const apiHost = 'http://localhost:3030/';
-
-
-// CREATE TLDR
-export const createTldr = (data) => ({
-	[RSAA]: {
-		endpoint: `${apiHost}tldrs/`,
-		method: 'POST',
-		headers: { 'Accept': 'application/json', 'Content-Type': 'application/json' },
-		body: JSON.stringify(data),
-		types: ["CREATE_TLDR","CREATE_TLDR_SUCCESS", "CREATE_TLDR_FAILURE"]
- 	}
-});
-
-// PATCH TLDR
-export const patchTldr = (id, data) => ({
-	[RSAA]: {
-		endpoint: `${apiHost}tldrs/${id}`,
-		method: 'PATCH',
-		headers: { 'Accept': 'application/json', 'Content-Type': 'application/json' },
-		body: JSON.stringify(data),
-		types: ["PATCH_TLDR","PATCH_TLDR_SUCCESS", "PATCH_TLDR_FAILURE"]
- 	}
-});
-
-// VALIDATE TLDR FAILURE
-export const updateErrorTldr = (error) => ({
-    type: 'UPDATE_ERROR_TLDRVERSION',
-    payload: error
-})
-
-// FETCH TLDRS
-export const fetchTldrs = () => ({
-	[RSAA]: {
-		types: ["FETCH_TLDRS", "FETCH_TLDRS_SUCCESS", "FETCH_TLDRS_FAILURE"],
-		endpoint: `${apiHost}tldrs`,
-		method: 'GET',
- 	}
-});
-
-export const fetchTldr = (id) => ({
-	[RSAA]: {
-		types: ["FETCH_TLDR", "FETCH_TLDR_SUCCESS", "FETCH_TLDR_FAILURE"],
-		endpoint: `${apiHost}tldrs/${id}`,
-		method: 'GET',
- 	}
-});
-
 
 
 // LOG IN WITH FEATHERS CLIENT
@@ -60,7 +12,6 @@ export const logInSuccess = (authentication) => ({
 export const logOut = () => ({
 	type: 'LOG_OUT'
 });
-
 
 
 /*
--- packages/frontend/store/index.js
+++ packages/frontend/store/index.js
@@ -1,36 +1,17 @@
 //https://github.com/vercel/next.js/tree/canary/examples/with-redux-wrapper
 
-import { createStore, applyMiddleware } from 'redux';
-import thunkMiddleware from 'redux-thunk'
-import { apiMiddleware, isRSAA, RSAA } from 'redux-api-middleware';
+import { createStore } from 'redux';
 import { HYDRATE, createWrapper } from 'next-redux-wrapper';
 import reducer from '../reducers';
 
 
-const authMiddleware = ({getState, dispatch}) => next => action => {
-   const token = (getState().authentication && getState().authentication.token) ? getState().authentication.token : '';
-   if (action[RSAA]) {
-     action[RSAA].headers = {
-       Authorization: 'Bearer ' + token,
-       ...action[RSAA].headers
-     }
-   }
-   return next(action)
- }
+
 
 
 const makeStore = (initialState, options) => {
 
-   const createdStore =  createStore(
-     reducer,
-     applyMiddleware(
-       thunkMiddleware,
-       authMiddleware,
-       apiMiddleware,
-     )
-   );
-   console.log(createdStore);
+   const createdStore =  createStore( reducer );
    return createdStore;
 };
 
-export const wrapper = createWrapper(makeStore)
+export const wrapper = createWrapper( makeStore )
