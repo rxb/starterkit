2aaae673081bc1552932cc853b9b347a03da0258
Richard <boenigk@gmail.com>
2021-03-16 18:37:12 -0400

    refactoring swr

--- packages/frontend/swr/index.js
+++ packages/frontend/swr/index.js
@@ -21,9 +21,9 @@ export const getUserUrl = (id='') => `${apiHost}/users/${id}`;
 
 
 // REQUEST
-// vanilla fetch only throws js error for js problems
-// this will throw js error for js AND http problems
-// also provides some syntactic sugar for data and token 
+// Vanilla fetch only throws js error for js problems.
+// This will throw js error for js AND http problems
+// It also provides some syntactic sugar for data and token 
 export function request(url, options) {
    const buildOptions = (options = {}) => {
       const {
@@ -68,12 +68,20 @@ export function request(url, options) {
    });
 }
 
+// FETCHER
+// Objects need to stay out of the useSWR key due to the way it compares keys.
+// That means token needs to be passed in as a string arg, not the options object expected by request()
+// This fn bridges what useSWR needs and what request needs
 export const fetcher = (url, token='') => request(url, {token: token});
 
+// BUILDQS
 export const buildQs = (params) => {
    return (params) ? "?"+Object.keys(params).map(key => key + '=' + params[key]).join('&') : '';
 }
 
+// PARSEPAGEOBJ
+// reorganizes pagination objects returned by feathers and outputted by useSWR
+// into something a little flatter and easier to use 
 export const parsePageObj = (swr) => {
    const {data, ...meta} = swr.data || {}; 
    return { 
@@ -82,5 +90,3 @@ export const parsePageObj = (swr) => {
       meta: meta
    }
 }
-
-
