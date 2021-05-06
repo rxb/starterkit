e4c2cbdb7d293518efdae1fcff4db40d8784e4f1
Richard <boenigk@gmail.com>
2021-05-06 13:09:08 -0400

    tldr votes service

--- packages/backend/src/services/tldrs_votes/tldrs_votes.hooks.js
+++ packages/backend/src/services/tldrs_votes/tldrs_votes.hooks.js
@@ -3,6 +3,27 @@ const { authenticate } = require('@feathersjs/authentication').hooks;
 const { setField } = require('feathers-authentication-hooks');
 const { iff, isProvider } = require('feathers-hooks-common');
 
+const updateVoteTally = async(context) => {
+  console.log(context.data);
+  const upvotes = await context.service.find({query: {
+    vote: 1,
+    tldrId: context.result.tldrId,
+    $limit: 0 // count
+  }});
+  const downvotes = await context.service.find({query: {
+    vote: -1,
+    tldrId: context.data.tldrId,
+    $limit: 0 // count
+  }});
+  //console.log(upvotes);
+  //console.log(downvotes);
+  const voteQuantity = upvotes.total + downvotes.total;
+  const voteResult = upvotes.total - downvotes.total;
+  const votePositivity = (voteQuantity > 0) ? voteResult / voteQuantity : 0;
+  //console.log(`quantity: ${voteQuantity} result: ${voteResult} positivity: ${votePositivity} `)
+  return context;
+}
+
 module.exports = {
   before: {
     all: [],
@@ -14,6 +35,10 @@ module.exports = {
         from: 'params.user.id',
         as: 'data.userId'
       }),
+      (context) => {
+        console.log(context.data);
+        return context;
+      },
       // clear vote before doing another vote
       // other option would be diverting to patch/update
       async (context) => {
@@ -32,7 +57,11 @@ module.exports = {
       setField({
         from: 'params.user.id',
         as: 'data.userId'
-      }) // this functions as security, must be owned by user, requires 'multi' in service
+      }), // this functions as security, must be owned by user, requires 'multi' in service
+      (context) => {
+        console.log(context.data);
+        return context;
+      }
     ]
   },
 
@@ -40,10 +69,14 @@ module.exports = {
     all: [],
     find: [],
     get: [],
-    create: [],
+    create: [
+      updateVoteTally
+    ],
     update: [],
     patch: [],
-    remove: []
+    remove: [
+      updateVoteTally
+    ]
   },
 
   error: {
--- packages/backend/src/services/tldrs_votes/tldrs_votes.service.js
+++ packages/backend/src/services/tldrs_votes/tldrs_votes.service.js
@@ -7,7 +7,7 @@ module.exports = function (app) {
   const options = {
     Model: createModel(app),
     paginate: app.get('paginate'),
-    multi: ['patch','remove']
+    multi: ['remove']
   };
 
   // Initialize our service with any options it requires
