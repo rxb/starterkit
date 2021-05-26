9b4d711d39e869288d4957c07e6a71d7eb11c0aa
Richard <boenigk@gmail.com>
2021-05-26 12:50:44 -0400

    searchush

---
+++ packages/backend/sequelize/migrations/20210506193831-create-categories.js
@@ -0,0 +1,41 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('categories', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      urlKey: {
+        type: Sequelize.TEXT,
+        unique: true
+      },
+      tldrCount: {
+        type: Sequelize.INTEGER,
+        defaultValue: 0
+      },
+      name: {
+        type: Sequelize.TEXT
+      },
+      description: {
+        type: Sequelize.TEXT
+      },
+      keywords: {
+        type: Sequelize.TEXT
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('categories');
+  }
+};
